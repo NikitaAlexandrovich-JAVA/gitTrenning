bfd44ab5f172519170d01dc7db88d26c9ab09ca6
NikitaAlexandrovich-JAVA <pukaievich00@mail.ru>
2025-01-10 02:23:38 +0300

    удалили нернужное

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
-        System.out.println("Hello Earth!!");
+
 
         for (int a=0;a<10;a++){
             System.out.println(a);
